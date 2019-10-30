cfab0d06550e31196a5441769ba8903133044abf
OnlyTanner <lisonbeetanner@gmail.com>
2019-10-30 14:58:56 -0700

    USnone T31 Added software implementation based on feature branches.
    Contains functionality to illuminate the LEDs based on the phase and output the turn direction over USART.

---
+++ QuadratureDecoder-Software.X/main.c
@@ -0,0 +1,75 @@
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include <stdlib.h>
+#include <util/delay.h>
+
+/*
+    Main application
+*/
+int main(void)
+{
+    /* Initializes MCU, drivers and middleware */
+    SYSTEM_Initialize();
+
+    int oldA = IO_PE0_GetValue(); //get the initial value of clkPin
+    int oldB = IO_PE1_GetValue() >> 1; //get the initial value of dtPin
+    unsigned int state = 2;
+
+    /* Replace with your application code */
+    while (1){
+        unsigned char result = 0;
+        unsigned char newA = IO_PE0_GetValue();//read the value of clkPin to newA
+        unsigned char newB = IO_PE1_GetValue() >> 1;//read the value of dtPin to newB
+        //Sense falling edge of clkPin
+        if (oldA == 0 && newA == 1) {
+            if (oldB == newB && oldB == 0) {
+                printf("Clockwise turn detected");
+            } else if (oldB == newB && oldB == 1) {
+                printf("Counter-clockwise turn detected");
+            }
+        }
+        if (oldA == 1 && newA == 0) {
+            if (oldB == newB && oldB == 1) {
+                printf("Clockwise turn detected");
+            } else if (oldB == newB && oldB == 0) {
+                printf("Counter-clockwise turn detected");
+            }
+            //increment the state or decrement the state based on result, 
+            //state can never be negative or greater then 31
+            result = (oldB * 8) - 1; 
+        }
+        //increment the state or decrement the state based on result, 
+        //state can never be negative or greater then 7
+        state = (state + result) % 32;
+        //store the new state of clkPin and dtPin for the next cycle through
+        oldA = newA;
+        oldB = newB;
+        //display the state to the connected lights
+        VPORTF.OUT = state;
+    }
+}
+/**
+    End of File
+*/
---
+++ QuadratureDecoder-Software.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=QuadratureDecoder-Software.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/QuadratureDecoder-Software.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=quadraturedecoder-software.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/quadraturedecoder-software.x.tar
