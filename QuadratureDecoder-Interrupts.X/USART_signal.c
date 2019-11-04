3baf9c136010cfc3997be764b272d6e55d6ddfd4
jayguest <46973031+jayguest@users.noreply.github.com>
2019-11-03 20:28:35 -0700

    US33 Task42 skeleton code for a usart class to send decoder signals
    This is just the bare file to be edited, i'm testing the integration of new c files into the project before writing the actual code

---
+++ QuadratureDecoder-Interrupts.X/USART_signal.c
@@ -0,0 +1,8 @@
+// Skeleton code file for USART of decoder signals
+
+// basic form for method to send USART messages
+// needs: input signals from the decoder
+// returns: a data packet which can be send over USART to the user system
+uint8_t sendDecoderSignals(){
+	
+}
