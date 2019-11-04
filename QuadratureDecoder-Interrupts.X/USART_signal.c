976a89d6e55a707daec5eef6f0fd03368b1db396
ZGuess <zane_g45@hotmail.com>
2019-11-03 23:38:06 -0700

    Task 43 Started to implement a function that formats input data to unsigned char array
    Testing is needed but this is a building block to be able to continue. Need to understand polling better before fully commiting a viable solution to this task

--- QuadratureDecoder-Interrupts.X/USART_signal.c
+++ QuadratureDecoder-Interrupts.X/USART_signal.c
@@ -6,3 +6,25 @@
 uint8_t sendDecoderSignals(){
 	
 }
+
+
+/*
+ Working on task 43 attempting to find a solution that works 
+ 
+ * The current solution is based off research and has not yet been fully tested.
+ * Ideally using this function then we could do something like - 
+ * 
+  while(1) {
+  line[i] = getchar();
+  if (line[i] == '\n') break;
+  i++;
+}
+line[i] = 0;
+ * which would get each char line by line until \0 is reached 
+ * further testing is required.
+ */
+uint8_t uart_getchar(void) {
+  while ((UCSRA & (1 << RXC)) == 0) {
+  }
+  return UDR;
+}
