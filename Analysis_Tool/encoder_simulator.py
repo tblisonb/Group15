d1273a40bdd65a17951ae57aa1257003d412c875
OnlyTanner <lisonbeetanner@gmail.com>
2019-11-19 13:33:40 -0700

    US65 T77 Added separate script to simulate an encoder.
    User specifies the number of rotations, direction, and velocity, and the results are measured.

--- Analysis_Tool/encoder_simulator.py
+++ Analysis_Tool/encoder_simulator.py
@@ -4,20 +4,23 @@ import time
 from gpiozero import LED
 
 NUM_STEPS = 20
+channel_A = 0
+channel_B = 0
 old_channel_A = 0
 old_channel_B = 0
+prev_change = 'B'
 output_A = LED("GPIO16")
 output_B = LED("GPIO20")
 
 """ Initialize serial I/O with ATmega4809 and call parser function. """
-def run(port, baud, timeout):
+def run(port, baud, timeout, dir, num_rotations, vel):
     print("Attempting to open port " + port + " at " + baud + " Hz.")
     ser = serial.Serial(port, baud, timeout=5)      # open port
     prev = time.time()                 # track time after opening
-    while time.time() - prev < timeout:
-        # rotate(0.1, 50, CC, ser)
-        # delay 50 ms
-        prev = time.time()  # track time after opening
+    print("Expected: Direction = " + str(dir) + ", Num Rotations = " + str(num_rotations) + ", Velocity = " + str(vel))
+    for i in range(num_rotations):
+        rotate(vel, dir, ser)
+        prev = time.time()  # start rotation
         s = ser.read(1)     # read char
         cur = time.time()   # end time
         inst_v = (float(1) / float(NUM_STEPS)) / (cur - prev) # compute velocity of turn
@@ -72,74 +75,44 @@ def display(direction, velocity):
         
 """ Simulate a rotary encoder turn with a certain speed, number of rotations,
     in either the clockwise or counter-clockwise direction. """
-def rotate(velocity, num_rotations, direction, ser):
-    channel_A = 0
-    channel_B = 0
+def rotate(velocity, direction, ser):
     if direction == 'CW':
-        for i in range(num_rotations):
+        if prev_change == 'B':
             channel_A ^= 1
             output_A.value = channel_A
+            prev_change = 'A'
             time.sleep(float(velocity) / float(NUM_STEPS))
+        else:
             channel_B ^= 1
             output_B.value = channel_B
+            prev_change = 'B'
             time.sleep(float(velocity) / float(NUM_STEPS))
     elif direction == 'CC':
-        for i in range(num_rotations):
+        if prev_change == 'B':
             channel_B ^= 1
             output_B.value = channel_B
+            prev_change = 'A'
             time.sleep(float(velocity) / float(NUM_STEPS))
+        else:
             channel_A ^= 1
             output_A.value = channel_A
+            prev_change = 'B'
             time.sleep(float(velocity) / float(NUM_STEPS))
     old_channel_A = channel_A
     old_channel_B = channel_B
-    
-def run_simulation():
-    
 
 def main():
-    """## -- TEST parse_inputs -- ##
-    result0 = parse_inputs(0) # no turn
-    result1 = parse_inputs(1) # counter-clockwise
-    result2 = parse_inputs(2) # clockwise
-    result3 = parse_inputs(3) # invalid
-    result4 = parse_inputs(4) # clockwise
-    result5 = parse_inputs(5) # no turn
-    result6 = parse_inputs(6) # invalid
-    result7 = parse_inputs(7) # counter-clockwise
-    result8 = parse_inputs(8) # counter-clockwise
-    result9 = parse_inputs(9) # invalid
-    result10 = parse_inputs(10) # no turn
-    result11 = parse_inputs(11) # clockwise
-    result12 = parse_inputs(12) # invalid
-    result13 = parse_inputs(13) # clockwise
-    result14 = parse_inputs(14) # counter-clockwise
-    result15 = parse_inputs(15) # no turn
-    assert(result0 == 'NT')
-    assert(result1 == 'CC')
-    assert(result2 == 'CW')
-    assert(result3 == 'IV')
-    assert(result4 == 'CW')
-    assert(result5 == 'NT')
-    assert(result6 == 'IV')
-    assert(result7 == 'CC')
-    assert(result8 == 'CC')
-    assert(result9 == 'IV')
-    assert(result10 == 'NT')
-    assert(result11 == 'CW')
-    assert(result12 == 'IV')
-    assert(result13 == 'CW')
-    assert(result14 == 'CC')
-    assert(result15 == 'NT')"""
-    
-    if len(sys.argv) == 4:          # check for correct command line args
+    if len(sys.argv) == 6:          # check for correct command line args
         port = sys.argv[1]          # assign arg value for port
         baud = sys.argv[2]          # assign arg value for baud
         time = sys.argv[3]          # assign arg value for timeout
-        run(port, baud, time)
+        dir = sys.argv[4]           # direction of turn
+        num_rotations = sys.argv[5] # number of rotations to trigger
+        vel = sys.argv[6]           # velocity of the turn
+        run(port, baud, time, dir, num_rotations, vel)
     else:
         print("Incorrect arguments provided")
-        print("Format: python analysis_tool.py [port] [baud] [timeout]")
+        print("Format: python encoder_simulator.py [port] [baud] [timeout] [direction] [num rotations]")
     
 if __name__ == "__main__":
     main()
