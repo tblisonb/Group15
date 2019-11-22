1484db87bb29baf3b183cb640d839e27dd9a0ea2
Jason Guest <jaguest@asu.edu>
2019-11-21 20:41:32 -0700

    US66 Task 72
    Removed USART0 as it was not being used and took pins needed by TCA0. Added TCA0 to the project, as the stopwatch feature cannot be used and the watch window is not a good option for performance.  Began a text document to outline how to use the timer coutner peripherals

---
+++ QuadratureDecoder-Software.X/build/default/debug/mcc_generated_files/src/usart1.o.d
@@ -0,0 +1,75 @@
+build/default/debug/mcc_generated_files/src/usart1.o.d \
+ build/default/debug/mcc_generated_files/src/usart1.o: \
+ mcc_generated_files/src/usart1.c \
+ mcc_generated_files/src/../include/usart1.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdbool.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdio.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdint.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ mcc_generated_files/src/../include/../utils/compiler.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\sfr_defs.h \
+ C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.25/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4809.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\xmega.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\builtins.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdlib.h \
+ mcc_generated_files/src/../include/../utils/interrupt_avr8.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h \
+ mcc_generated_files/src/../include/../utils/atomic.h \
+ mcc_generated_files/src/../include/../config/clock_config.h
+
+mcc_generated_files/src/../include/usart1.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+mcc_generated_files/src/../include/../utils/compiler.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\sfr_defs.h:
+
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.25/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4809.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\builtins.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdlib.h:
+
+mcc_generated_files/src/../include/../utils/interrupt_avr8.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h:
+
+mcc_generated_files/src/../include/../utils/atomic.h:
+
+mcc_generated_files/src/../include/../config/clock_config.h:
