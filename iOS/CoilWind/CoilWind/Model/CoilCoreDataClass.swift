5fc31a68803c81299d0e819f580e8a28e257027a
ZGuess <zane_g45@hotmail.com>
2020-02-13 19:04:32 -0700

    Task 148 making core data class
    Begun writing the functionality of the database. as in adding, removing, get functions etc. This will allow for persistance in testing and for further implementations

---
+++ iOS/CoilWind/CoilWind/Model/CoilCoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoilCoreDataClass.swift
+//  CoilWind
+//
+//  Created by Zane Guess on 2/13/20.
+//  Copyright © 2020 Capstone Team 15. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Coil)
+public class Coil: NSManagedObject {
+
+}
---
+++ iOS/CoilWind/CoilWind/Model/CoilCoreDataDB.swift
@@ -0,0 +1,136 @@
+//
+//  CoilCoreDataDB.swift
+//  CoilWind
+//
+// --------------------------------------------------------------------------------
+// This class is to add functionality to the database allowing for dynamic changes
+// to be made depending on the user input/ choices
+// --------------------------------------------------------------------------------
+//
+//  Created by Zane Guess on 2/11/20.
+//  Copyright © 2020 Capstone Team 15. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+// data base class
+public class CoilCoreDataDB{
+    
+    var appDel:AppDelegate?
+    var mContext:NSManagedObjectContext?
+    
+    // initialize the database
+    init(){
+        appDel = (UIApplication.shared.delegate as! AppDelegate)
+        mContext = appDel!.persistentContainer.viewContext
+    }
+    
+    // check whether a coil setting exists in the data storage
+    func coilExists(withName:String)->Bool {
+        var ret:Bool = false
+        let selectRequest:NSFetchRequest<Coil> = Coil.fetchRequest()
+        selectRequest.predicate = NSPredicate(format:"name == %@",withName)
+        
+        // checking if the name exists in database
+        do{
+            let results = try mContext!.fetch(selectRequest)
+            if results.count > 0 {
+                ret = true
+            }
+        } catch let error as NSError{
+            NSLog("Error selecting coil \(withName). Error: \(error)")
+        }
+        return ret
+    }
+    
+    // get array of all names in the data base
+    func getNames() -> [String] {
+        var ret:[String] = [String]()
+        let fetchStuds:NSFetchRequest<Coil> = Coil.fetchRequest()
+        do{
+            let results = try mContext!.fetch(fetchStuds)
+            for aCoil in results {
+                ret.append((aCoil as AnyObject).value(forKey:"name") as! String)
+            }
+        } catch let error as NSError {
+            print("error selecting all coils: \(error)")
+        }
+        return ret.sorted()
+    }
+    
+    // add a coil setting to the database
+    func addCoil(_ name:String, color:String, length:Double, gauge:Double, quantity:Int) -> Bool {
+        var ret:Bool = false
+        if !coilExists(withName: name) {
+            let entity = NSEntityDescription.entity(forEntityName: "Coil", in: mContext!)
+            let aCoil = NSManagedObject(entity: entity!, insertInto: mContext)
+            aCoil.setValue(name, forKey:"name")
+            aCoil.setValue(color, forKey:"color")
+            aCoil.setValue(length, forKey:"length")
+            aCoil.setValue(gauge, forKey:"gauge")
+            aCoil.setValue(quantity, forKey:"quantity")
+ 
+            ret = true
+        }
+        return ret
+    }
+    
+
+    // get a specific coil setting from data base with all associated description values of the coil
+    func getCoil(_ name:String) -> (name:String, color:String, length:Double, gauge:Double, quantity:Int) {
+        
+        var aCoil:(name:String, color:String, length:Double, gauge:Double, quantity:Int) = ("name","",0.0,0.0,0)
+        
+        let fetchACoil:NSFetchRequest<Coil> = Coil.fetchRequest()
+        fetchACoil.predicate = NSPredicate(format:"name == %@",name)
+        do{
+            let results = try mContext!.fetch(fetchACoil)
+            if results.count > 0 {
+                let color = (results[0] as AnyObject).value(forKey: "color") as? String
+                let length = (results[0] as AnyObject).value(forKey: "length") as? Double
+                let gauge = (results[0] as AnyObject).value(forKey: "gauge") as? Double
+                let quantity = (results[0] as AnyObject).value(forKey: "quantity") as? Int
+
+                
+                aCoil = (name,color,length,gauge,quantity) as! (name: String, color: String, length: Double, gauge: Double, quantity: Int)
+            }
+        }catch let error as NSError{
+            print("error getting coil \(name), error \(error)")
+        }
+        return aCoil
+    }
+    
+
+    // delete Coil setting from data base
+    func deleteCoil(name:String) -> Bool {
+        var ret:Bool = false
+        let selectRequest:NSFetchRequest<Coil> = Coil.fetchRequest()
+        selectRequest.predicate = NSPredicate(format:"name == %@",name)
+        do{
+            let results = try mContext!.fetch(selectRequest)
+            if results.count > 0 {
+                mContext!.delete(results[0] as NSManagedObject)
+                //try mContext?.save()
+                ret = true
+            }
+        } catch let error as NSError{
+            NSLog("error deleting coil \(name). Error \(error)")
+        }
+        return ret
+    }
+
+    
+    // save the context of the database
+    func saveContext() -> Bool {
+        var ret:Bool = false
+        do{
+            try mContext!.save()
+            ret = true
+        }catch let error as NSError{
+            print("error saving context \(error)")
+        }
+        return ret
+    }
+}
