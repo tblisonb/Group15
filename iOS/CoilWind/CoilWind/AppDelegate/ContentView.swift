5dbbfe4a52ff0113bb0ef2a7c24af613de2ae282
ZGuess <zane_g45@hotmail.com>
2020-02-15 21:27:31 -0700

    Task 152/153 making views for application
    Made changes to view from the wireframe to have a better design. The views are complete will continue working on the controllers to give the functionality to the system.

--- iOS/CoilWind/CoilWind/AppDelegate/ContentView.swift
+++ iOS/CoilWind/CoilWind/AppDelegate/ContentView.swift
@@ -5,7 +5,7 @@
 //  Created by Zane Guess on 2/8/20.
 //  Copyright © 2020 Capstone Team 15. All rights reserved.
 //
-
+/*
 import SwiftUI
 
 struct ContentView: View {
@@ -19,3 +19,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+*/
