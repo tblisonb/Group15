94a57707e7374aa69621a72ab9be41fa02d344cc
jjgilbe3 <jjgilbe3@asu.edu>
2020-02-18 21:17:26 -0700

    US139 Task 146 - Creating the initial project for the atMega3208 Bluetooth/CCL implementation

---
+++ Bluetooth_CCL.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Bluetooth_CCL.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Bluetooth_CCL.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=bluetoothccl.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/bluetoothccl.x.tar
