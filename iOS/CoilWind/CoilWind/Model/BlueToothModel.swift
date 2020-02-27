6be9fcf532f175b4df05045dee3449bd004649ba
ZGuess <zane_g45@hotmail.com>
2020-02-27 16:52:52 -0700

    Started implementing core bluetooth framework
    Begun setting up the framework to allow for BLE connection. All set up will be completed; however, no tests can be done until the connecting application is complete/ has usuable UUID's

---
+++ iOS/CoilWind/CoilWind/Model/BlueToothModel.swift
@@ -0,0 +1,15 @@
+//
+//  BlueToothModel.swift
+//  CoilWind
+//
+//  Created by Zane Guess on 2/27/20.
+//  Copyright © 2020 Capstone Team 15. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+public class BlueToothModel
+{
+    
+}
