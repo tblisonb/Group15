808c35992be520f6d4d376f99b2d53472d82e501
Jason Guest <jaguest@asu.edu>
2020-02-28 20:33:07 -0700

    Task 179 - created two method stubs for lcd display functionality
    Need to do some more research into the use of this actuator before I can write good library code

--- AVRBLE_Setup.X/i2c_lcd_lib.c
+++ AVRBLE_Setup.X/i2c_lcd_lib.c
@@ -3,3 +3,15 @@
  * Author: Jason Guest
  * Version: February 2020
  */
+
+
+/* The four part connection will be focused on primarily, as it leaves pins on the 
+   board available for our other sensors and actuators */
+
+void lcd_8port_out(){
+    
+}
+
+void lcd_16port_out(){
+    
+}
