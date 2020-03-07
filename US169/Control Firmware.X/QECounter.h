34b3a89813330c673788cf8f27167325105ca5c8
Manolito Ramirez <1manoramirez@gmail.com>
2020-03-06 22:14:27 -0700

    Task172
    added initial firmware control code

---
+++ US169/Control Firmware.X/QECounter.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   QECounter.h
+ * Author: Manolito Ramirez
+ *
+ * Created on March 6, 2020, 9:34 PM
+ */
+
+#ifndef QECOUNTER_H
+#define	QECOUNTER_H
+
+void counter_increment();
+int counter_get();
+
+#endif	/* QECOUNTER_H */
+
