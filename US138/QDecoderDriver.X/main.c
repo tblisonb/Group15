82e2acb80e84d4a25c0d069a7d59f8cd89c504a2
OnlyTanner <lisonbeetanner@gmail.com>
2020-03-21 16:14:15 -0700

    US201 T204 Updated firmware to be inclusive of all actuators.

--- US138/QDecoderDriver.X/main.c
+++ US138/QDecoderDriver.X/main.c
@@ -22,82 +22,30 @@
 */
 #include "mcc_generated_files/include/pin_manager.h"
 #include <stdlib.h>
-#include "stepper.h"
-#include "RN487x.h"
+#include "wire.h"
 #include "mcc_generated_files/mcc.h"
 #include "mcc_generated_files/delay.h"
-
-void led_blink(int num_times) {
-    for (int i = 0; i < num_times; i++) {
-        IO_PF5_SetHigh();
-        DELAY_milliseconds(200);
-        IO_PF5_SetLow();
-        DELAY_milliseconds(200);
-    }
-    DELAY_milliseconds(800);
-}
 
 /*
     Main application
 */
 int main(void)
 {
-    /* Initializes MCU, drivers and middleware */
     SYSTEM_Initialize();
-    DELAY_milliseconds(RN487X_STARTUP_DELAY);
+    init_wire(); // initialize RN4870
     
-    RN487X_Init();              // initialize BLE HW module
-    DELAY_milliseconds(200);    // allow module to process commands
-    RN487X_EnterCmdMode();      // enter command mode
-    DELAY_milliseconds(200);    // processing time
-    uint8_t cmd_buf[16];        // command buffer
-    
-    // command to set the name of the device as "Coil_Wind"
-    cmd_buf[0] = 'S';
-    cmd_buf[1] = '-';
-    cmd_buf[2] = ',';
-    cmd_buf[3] = 'C';
-    cmd_buf[4] = 'o';
-    cmd_buf[5] = 'i';
-    cmd_buf[6] = 'l';
-    cmd_buf[7] = '_';
-    cmd_buf[8] = 'W';
-    cmd_buf[9] = 'i';
-    cmd_buf[10] = 'n';
-    cmd_buf[11] = 'd';
-    cmd_buf[12] = '\n';
-    cmd_buf[13] = '\r';
-    
-    RN487X_SendCmd(cmd_buf, 14);    // send the command to the BLE module
-    DELAY_milliseconds(200);        // processing time
-    
-    // command to enable "Transparent UART" service
-    cmd_buf[0] = 'S';
-    cmd_buf[1] = 'S';
-    cmd_buf[2] = ',';
-    cmd_buf[3] = 'C';
-    cmd_buf[4] = 'O';
-    cmd_buf[5] = '\n';
-    cmd_buf[6] = '\r';
-    
-    RN487X_SendCmd(cmd_buf, 7); // enable device info & T-UART services
-    DELAY_milliseconds(200);
-    RN487X_RebootCmd();         // reboot the device for the changes made
-    DELAY_milliseconds(RN487X_STARTUP_DELAY);
-    led_blink(2);               // ready signal
-    
-    int idx = 0;
-    uint8_t msg[16];
     while (1) {
-        if (RN487X_DataReady()) {
-            msg[idx++] = RN487X_Read();
-            cw_turn((volatile unsigned char*)&PORTE.OUT, 100, 10);
+        volatile uint8_t buffer[64];
+        read_data(buffer); // blocking call
+        /*
+         * The following is placeholder code for interpreting the input
+        for (unsigned int i = 0; i < num_types_of_wire; i++) {
+         for (unsigned int j = 0; j < num_of_single_type; j++) {
+          extrude(length_of_the_given_wire);
+         }
+         retract(length_to_release_wire);
+         while(flag); //wait for user to change type of wire and press continue
         }
-        if (idx == 15) {
-            led_blink(2);
-            RN487X_SendCmd(msg, 16);
-            idx = 0;
-            cc_turn((volatile unsigned char*)&PORTE.OUT, 300, 8);
-        }
+        */
     }
 }
--- US138/QDecoderDriver.X/wire.h
+++ US138/QDecoderDriver.X/wire.h
@@ -5,24 +5,34 @@
 
 // This is a guard condition so that contents of this file are not included
 // more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+#ifndef WIRE_TEMPLATE_H
+#define	WIRE_TEMPLATE_H
 
-#include <xc.h> // include processor files - each processor file is guarded.  
+#include "servo.h"
+#include "stepper.h"
+#include "RN487x.h"
+#include "mcc_generated_files/delay.h"
+#include "mcc_generated_files/include/pin_manager.h"
 
 #define EXTRUDER_GEAR_RADIUS 5 // mm
 #define ENCODER_NUM_STEPS 20 // steps for one full rotation
 #define STEP_RADIANS (62832 / ENCODER_NUM_STEPS) // angular displacement *= 10E-4
-#define STEPPER_PORT ((volatile unsigned char*) &PORTB)
-#define SERVO_PORT ((volatile unsigned char*) &PORTC)
-#define SERVO_BIT 0
+#define STEPPER_PORT ((volatile unsigned char*) &PORTE.OUT)
+#define SERVO_PORT ((volatile unsigned char*) &PORTA.OUT)
+#define SERVO_BIT 2
 #define STRIP_OFFSET_ANGLE 70
 #define CUT_OFFSET_ANGLE 90
+#define STEPPER_DELAY 25
 
 /*
- * Runs the stepper motor for a single step to extrude wire.
+ * Initializes system.
  */
-void extrude_min_step();
+void init_wire();
+
+/*
+ * Checks and reads data from BLE module.
+ */
+void read_data(volatile uint8_t* buffer);
 
 /*
  * Extrudes 'length' wire.
@@ -30,11 +40,6 @@ void extrude_min_step();
  * @param length Length of wire to be extruded in mm
  */
 void extrude(int length);
-
-/*
- * Runs the stepper motor for a single step to retract wire.
- */
-void retract_min_step();
 
 /*
  * Retracts 'length' wire.
@@ -53,5 +58,5 @@ void strip();
  */
 void cut();
 
-#endif	/* XC_HEADER_TEMPLATE_H */
+#endif	/* WIRE_TEMPLATE_H */
 
